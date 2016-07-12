6c61c2fee51a3d84099724621d76ef387d0a4806
Thanh Vu <vut@lafayette.edu>
2016-07-12 12:51:28 -0400

    Calculate the title's data for perspective transformation

--- scripts/trainImgGenerator.py
+++ scripts/trainImgGenerator.py
@@ -5,15 +5,27 @@ import random
 
 """ ============================================================================================================
 This script generates a set of images for training darknet from the ground database of 400 posters.
+For each transformation methods, keep in mind that the deep net should use the title of the posters to recognize the whole poster.
+
+The title of a poster is represent using the following parameters:
+    <x> <y> <width> <height>
+Where x, y, width, and height are relative to the image's width and height, with x and y are the location of the center of the object:
+    x = x_center / img_w
+    y = y_center / img_h
+    with = obj_w / img w
+    height = obj_h / img_h
 ============================================================================================================ """
 
 SRC_DIR = '../../../db-deepnet/srcPosters/'
 DST_DIR = '../../../db-deepnet/training/'
 if not os.path.exists(DST_DIR): os.mkdir(DST_DIR)
+LABELS_DIR = '../../../db-deepnet/labels/'
+if not os.path.exists(LABELS_DIR): os.mkdir(LABELS_DIR)
 
 NUM_OF_IMG = 2 # the total number is 400 ground images
 STD_SIZE = 500 
-
+TITLE_RATIO = 0.2 # is estimated to be the ratio of title's height/ poster's height
+PT_RANGE = range(3,31,27) # range(3,31,3)
 
 """ This method makes all image to have the same width as a way of standardizing their sizes """
 def sizeStandardize(img,std_width): 
@@ -35,7 +47,7 @@ def addOcclusions(img):
 	# (x-start, x-end, x-step). same for ys 
 	# The step is to make sure the occlusions are spaced out reasonably
 	xs = (0, w-1, occWidth/4) 
-	ys = (int(h*0.2), int(h*0.5), int(h*0.1)) 
+	ys = (int(h*TITLE_RATIO), int(h*0.5), int(h*0.1)) 
 	
 	# Generate occlusions
 	for it in range(0,numOfOcc):
@@ -89,18 +101,31 @@ def perspectiveTransform (img, (r1,r2,r3,r4)):
 
 		# the original 4 points (corners) of the image to transform
 		src_points = np.float32([ [0,0], [w,0],
-														  [0,h], [w,h] ])
+		                          [0,h], [w,h] ])
 		# the corresponding new 4 location to transform original points to
 		y1 = 0 + int(h*r1) ; y2 = 0 + int(h*r2)
 		y3 = h - int(h*r3) ; y4 = h - int(h*r4)
 		dst_points = np.float32([ [0,y1], [w,y2],
-											        [0,y3], [w,y4] ])
+		                          [0,y3], [w,y4] ])
 		
 		# compute the transform matrix and apply it
 		M = cv2.getPerspectiveTransform(src_points,dst_points)
-		dst = cv2.warpPerspective(img,M,(w,h))
+		ptImg = cv2.warpPerspective(img,M,(w,h))
 		
-		return dst
+		# compute the title box for training detection
+		minY = min(y1,y2)
+		maxY1 = y1 + int((y3-y1)*TITLE_RATIO)
+		maxY2 = y2 + int((y4-y2)*TITLE_RATIO)
+		maxY = max(maxY1,maxY2) # the lowest corner of the title area
+		
+		x = 0.5 # x_center / img_w (since the title's width = the poster's width)
+		y = (minY + maxY)*0.5 / ptImg.shape[0] # y_center / img_h
+		width = 1 # obj_w / img w (since the title's width = the poster's width)
+		height = float(maxY - minY) / ptImg.shape[0] # obj_h / img_h
+		
+		tBox = (x, y, width, height)
+		
+		return (ptImg,tBox)
 	
 def ratiosToStr(tupleR):
 	out = ''
@@ -129,21 +154,34 @@ for i in range (0,NUM_OF_IMG):
 		
 		### Perspective Transformation
 		# generate the ratios of left and right edges
-		PT_RANGE = [x*0.01 for x in range(3,31,27)]
-# 		PT_RANGE = [x*0.01 for x in range(3,31,3)]
-		rightRatios = [(r,0,r,0) for r in PT_RANGE]
-		leftRatios  = [(0,r,0,r) for r in PT_RANGE]
+		ptRange = [x*0.01 for x in PT_RANGE]
+		rightRatios = [(r,0,r,0) for r in ptRange]
+		leftRatios  = [(0,r,0,r) for r in ptRange]
 		ratios = rightRatios + leftRatios
 		
 		for rs  in ratios:
-			imgT = perspectiveTransform(img,rs)	
-			if imgT != None:
+			ptOut = perspectiveTransform(img,rs)	
+			if ptOut != None:
+				imgT = ptOut[0]
+				titleBox = ptOut[1]
+				
+				## Save the training image
 				# name format: groundIdx_trans_transType_trainIdx.jpg
-				path_out = DST_DIR + `i`.zfill(3) + '_ptrans_' + ratiosToStr(rs) +'_'+ `cnt`.zfill(6) +'.jpg'
-				cv2.imwrite(path_out,imgT)
-				print 'Created ' + path_out
+				img_out = DST_DIR + `cnt`.zfill(6) +'_'+ `i`.zfill(3) +'_ptrans_'+ ratiosToStr(rs)  +'.jpg'
+				cv2.imwrite(img_out,imgT)
+				print 'Created ' + img_out
+				
+				## Save the label file
+				label_out = LABELS_DIR + `cnt`.zfill(6) +'.txt'
+				f = open(label_out,'w')
+				line = `i` + ' ' + `titleBox`.strip('()').replace(',','')
+				f.write(line)
+				f.close()
+				print 'Created ' + label_out
+				
 			else:
 				print 'Fail to create' + path_out
+				
 			cnt += 1
 
 	
