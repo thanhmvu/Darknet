15987d3b1ce8e90bb07ad1fc84ec4a08179d6ca4
Thanh Vu <vut@lafayette.edu>
2016-09-18 00:46:27 -0400

    Add scripts to display detection results

---
+++ scripts/displayResults.py
@@ -0,0 +1,91 @@
+import glob
+
+def css():
+	f = open("style.css","w")
+	contents = """
+	<style >
+	table {
+	    font-family: arial, sans-serif;
+	    border-collapse: collapse;
+	    width: 100%;
+	}
+
+	td, th {
+		white-space: nowrap;
+	    border: 1px solid #dddddd;
+	    text-align: left;
+	    padding: 8px;
+	}
+
+	tr:nth-child(even) {
+	    background-color: #dddddd;
+	}
+	</style>
+	"""
+
+	f.write(contents)
+
+
+def html(contents):
+	f = open("results.html","w")
+	
+	display = """
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	""" + contents + "</body>\n</html>\n"
+
+	f.write(display)
+
+def htmlImg(img, borderColor):
+	return "<img src=\""+ img + "\" alt=\"Class " + img + "\" style=\"height:200px; border:3px solid "+ borderColor +";\">"
+
+def imgIdx(img):
+	return img.split("/")[-1].split(".")[0]
+
+def display(resultDir,trainDir):
+	resultTable = """
+	<table>
+	  <tr>
+	    <th>Class Index</th>
+	    <th>Correct Images</th>
+	    <th>Training Image</th>
+	    <th>Test Images</th>
+	  </tr>
+	"""
+
+	totalCorrectImgs = 0
+	totalIncorrectImgs = 0
+
+	for folder in glob.glob(resultDir + "*"):
+		classIdx = folder.split("/")[-1]
+		trainImg = trainDir + classIdx + ".jpg";
+		correctImgs = glob.glob(folder + "/1/*");
+		incorrectImgs = glob.glob(folder + "/0/*");
+		resultTable += """
+		<tr>
+			<th>"""+ classIdx +"""</th>
+			<th>"""+ `len(correctImgs)` +"""</th>
+			<th>"""+ htmlImg(trainImg,"transparent") +"""</th>
+			<th>"""+ " ".join([imgIdx(img) +htmlImg(img,"blue") for img in correctImgs]) +" "+ " ".join([imgIdx(img) +htmlImg(img,"red") for img in incorrectImgs]) +"""</th>
+		</tr>
+		"""
+		totalCorrectImgs += len(correctImgs)
+		totalIncorrectImgs += len(incorrectImgs)
+
+	accuracy = totalCorrectImgs*1.0/(totalCorrectImgs +	totalIncorrectImgs)
+	resultTable += "</table>\n"
+	contents = "Total accuracy: " +`accuracy*100` +"%<br><br>" +resultTable
+	
+	css()
+	html(contents)
+
+
+
+# ============================== Main ==============================
+display("./results/","../../src/")
+
+
